72998b041bb03116e05d6b68856616fe24702a00
yangshao <3240457157@qq.com>
2024-12-20 20:13:46 +0800

    add c-code

---
+++ ctry.cpp
@@ -0,0 +1,13 @@
+﻿#include<stdio.h>
+int main()
+{
+	float a, b, c,maxx;
+	scanf_s("%f%f%f",&a,&b,&c);
+	maxx = a;
+	if (b > maxx)
+		maxx = b;
+	if (c > maxx)
+		maxx = c;
+	printf("%f",maxx);
+	return 0;
+}
